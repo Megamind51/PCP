555313db2a67f23e501942d31bb7fbc3e68e69ac
51Megamind <alexrone.rone3@gmail.com>
2019-11-04 16:29:51 +0000

    welele

--- src/main.c
+++ src/main.c
@@ -1,29 +1,74 @@
+/* Example program fragment to read a PAM or PNM image
+   from stdin, add up the values of every sample in it
+   (I don't know why), and write the image unchanged to
+   stdout. */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <pam.h>
 
+int min_vizinhos(unsigned int **matrix, int cols, int rows, int col, int row){
+  int min = 254;
+  if (row < rows-1 && col < cols-1 && col > 0 && row > 0){
+    if (matrix[row+1][col] < min)
+      min = matrix[row+1][col];
+    if (matrix[row+1][col+1] < min)
+      min = matrix[row+1][col+1];
+    if (matrix[row][col+1] < min)
+      min = matrix[row][col+1];
+    if (matrix[row-1][col] < min)
+      min = matrix[row-1][col];
+    if (matrix[row-1][col+1] < min)
+      min = matrix[row-1][col+1];
+    if (matrix[row+1][col-1] < min)
+      min = matrix[row+1][col-1];
+    if (matrix[row-1][col-1] < min)
+      min = matrix[row-1][col-1];
+    if (matrix[row][col-1] < min)
+      min = matrix[row][col-1];
+    }
+    return min +1;
+}
+
+
+int vizinhos_branco(unsigned int **matrix, int cols, int rows, int col, int row){
+  if (row < rows-1 && col < cols-1 && col > 0 && row > 0){
+    if(matrix[row+1][col] != 255 ||
+       matrix[row-1][col] != 255 ||
+       matrix[row+1][col+1] != 255 ||
+       matrix[row-1][col+1] != 255 ||
+       matrix[row+1][col-1] != 255 ||
+       matrix[row-1][col-1] != 255 ||
+       matrix[row][col+1] != 255 ||
+       matrix[row][col-1] != 255 )
+        return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char const *argv[]) {
-
-      if (argc < 2){
-        printf("Número de parâmetros inválidos\n");
-        exit(-1);
-      }
-
-      FILE *fptr;
-      if ((fptr = fopen(argv[1],"r")) == NULL){
-        printf("Erro ao abrir ficheiro");
-        // Program exits if the file pointer returns NULL.
-        exit(1);
-      }
-      char c;
-      c = fgetc(fptr);
-      while (c != EOF)
-        {
-        printf ("%c", c);
-        c = fgetc(fptr);
+      unsigned int row;
+      unsigned int col;
+      unsigned int rows, cols, maxval;
+      unsigned int **matrix;
+      pm_init(argv[0], 0);
+      matrix = pgm_readpgm(stdin, &cols,&rows,&maxval);
+        for(row = 0; row < rows; row++){
+          for (col = 0; col < cols; col++) {
+            // 255 -> Branco
+            if (matrix[row][col] == 255){
+              if(vizinhos_branco(matrix,cols,rows,col,row) ){
+                 matrix[row][col] = min_vizinhos(matrix, cols, rows, col,row);
+               }
+              //matrix[row][col] = (matrix[row][col] == 0) ? 255 : 0;
+            }
+          }
         }
+        FILE *fptr;
+        if ((fptr = fopen("testeoutput.pgm","w+")) == NULL){
+          printf("Erro ao abrir ficheiro");
+          // Program exits if the file pointer returns NULL.
+          exit(1);
+        }
+      pgm_writepgm(fptr, matrix, cols, rows, maxval, 1);
 
-      fclose(fptr);
 
 }
