92fd763ba567ae318ff2f469b779f860fc841b6e
51Megamind <alexrone.rone3@gmail.com>
2019-11-04 19:49:57 +0000

    1001 ifs de merda que ha uma merda bue simples de fazer mas sa foda agora e deixa arder, faz isso tu

--- src/main.c
+++ src/main.c
@@ -24,22 +24,149 @@ int min_vizinhos(unsigned int **matrix, int cols, int rows, int col, int row){
       min = matrix[row-1][col-1];
     if (matrix[row][col-1] < min)
       min = matrix[row][col-1];
+    }else if (row == 0 && col == 0){
+      if (matrix[row][col+1] < min)
+        min = matrix[row][col+1];
+      if (matrix[row+1][col+1] < min)
+        min = matrix[row+1][col+1];
+      if (matrix[row+1][col] < min)
+        min = matrix[row+1][col];
+    }else if (row == rows-1 && col == cols-1){
+      if (matrix[row][col-1] < min)
+        min = matrix[row][col-1];
+      if (matrix[row-1][col-1] < min)
+        min = matrix[row-1][col-1];
+      if (matrix[row-1][col] < min)
+        min = matrix[row-1][col];
+    }else if (row == rows-1 && col == 0){
+      if (matrix[row][col+1] < min)
+        min = matrix[row][col+1];
+      if (matrix[row-1][col+1] < min)
+        min = matrix[row-1][col+1];
+      if (matrix[row-1][col] < min)
+        min = matrix[row-1][col];
+    }else if (row == 0 && col == cols-1){
+      if (matrix[row][col-1] < min)
+        min = matrix[row][col-1];
+      if (matrix[row+1][col-1] < min)
+        min = matrix[row+1][col-1];
+      if (matrix[row+1][col] < min)
+        min = matrix[row+1][col];
+    }else if (row == 0){
+      if (matrix[row][col-1] < min)
+        min = matrix[row][col-1];
+      if (matrix[row][col+1] < min)
+        min = matrix[row][col+1];
+      if (matrix[row+1][col] < min)
+        min = matrix[row+1][col];
+      if (matrix[row+1][col+1] < min)
+        min = matrix[row+1][col+1];
+      if (matrix[row+1][col-1] < min)
+        min = matrix[row+1][col-1];
+    }else if (col == 0){
+      if (matrix[row+1][col] < min)
+        min = matrix[row+1][col];
+      if (matrix[row-1][col] < min)
+        min = matrix[row-1][col];
+      if (matrix[row][col+1] < min)
+        min = matrix[row][col+1];
+      if (matrix[row+1][col+1] < min)
+        min = matrix[row+1][col+1];
+      if (matrix[row-1][col+1] < min)
+        min = matrix[row-1][col+1];
+    }else if (col == cols-1){
+      if (matrix[row+1][col] < min)
+        min = matrix[row+1][col];
+      if (matrix[row-1][col] < min)
+        min = matrix[row-1][col];
+      if (matrix[row][col-1] < min)
+        min = matrix[row][col-1];
+      if (matrix[row+1][col-1] < min)
+        min = matrix[row+1][col-1];
+      if (matrix[row-1][col-1] < min)
+        min = matrix[row-1][col-1];
+
+    }else{
+      if (matrix[row][col-1] < min)
+        min = matrix[row][col-1];
+      if (matrix[row][col+1] < min)
+        min = matrix[row][col+1];
+      if (matrix[row-1][col] < min)
+        min = matrix[row-1][col];
+      if (matrix[row-1][col+1] < min)
+        min = matrix[row-1][col+1];
+      if (matrix[row-1][col-1] < min)
+        min = matrix[row-1][col-1];
     }
     return min +1;
 }
 
 
 int vizinhos_branco(unsigned int **matrix, int cols, int rows, int col, int row){
-  if (row < rows-1 && col < cols-1 && col > 0 && row > 0){
-    if(matrix[row+1][col] != 255 ||
-       matrix[row-1][col] != 255 ||
-       matrix[row+1][col+1] != 255 ||
-       matrix[row-1][col+1] != 255 ||
-       matrix[row+1][col-1] != 255 ||
-       matrix[row-1][col-1] != 255 ||
-       matrix[row][col+1] != 255 ||
-       matrix[row][col-1] != 255 )
-        return 1;
+    if (row < rows-1 && col < cols-1 && col > 0 && row > 0){
+      if(matrix[row+1][col] != 255 ||
+         matrix[row-1][col] != 255 ||
+         matrix[row+1][col+1] != 255 ||
+         matrix[row-1][col+1] != 255 ||
+         matrix[row+1][col-1] != 255 ||
+         matrix[row-1][col-1] != 255 ||
+         matrix[row][col+1] != 255 ||
+         matrix[row][col-1] != 255 )
+          return 1;
+    }else if(row == 0 && col == 0){
+      if(matrix[row][col+1] != 255 ||
+         matrix[row+1][col+1] != 255 ||
+         matrix[row+1][col] != 255)
+          return 1;
+    }else if(row == rows-1 && col == cols-1){
+      if(matrix[row][col-1] != 255 ||
+         matrix[row-1][col-1] != 255 ||
+         matrix[row-1][col] != 255)
+          return 1;
+
+    }else if(row == rows-1 && col == 0){
+      if(matrix[row][col+1] != 255 ||
+         matrix[row-1][col+1] != 255 ||
+         matrix[row-1][col] != 255)
+          return 1;
+
+    }else if(row == 0 && col == cols-1){
+      if(matrix[row][col-1] != 255 ||
+         matrix[row+1][col-1] != 255 ||
+         matrix[row+1][col] != 255)
+          return 1;
+
+    }else if(row == 0){
+      if(matrix[row][col-1] != 255 ||
+         matrix[row][col+1] != 255 ||
+         matrix[row+1][col] != 255 ||
+         matrix[row+1][col+1] != 255 ||
+         matrix[row+1][col-1] != 255)
+      return 1;
+
+    }else if(col == 0){
+      if(matrix[row+1][col] != 255 ||
+         matrix[row-1][col] != 255 ||
+         matrix[row][col+1] != 255 ||
+         matrix[row+1][col+1] != 255 ||
+         matrix[row-1][col+1] != 255)
+      return 1;
+
+    }else if(col == cols-1){
+      if(matrix[row+1][col] != 255 ||
+         matrix[row-1][col] != 255 ||
+         matrix[row][col-1] != 255 ||
+         matrix[row+1][col-1] != 255 ||
+         matrix[row-1][col-1] != 255)
+      return 1;
+
+    }else {
+      if(matrix[row][col-1] != 255 ||
+         matrix[row][col+1] != 255 ||
+         matrix[row-1][col] != 255 ||
+         matrix[row-1][col-1] != 255 ||
+         matrix[row-1][col+1] != 255)
+      return 1;
     }
     return 0;
 }
@@ -51,6 +178,7 @@ int main(int argc, char const *argv[]) {
       unsigned int **matrix;
       pm_init(argv[0], 0);
       matrix = pgm_readpgm(stdin, &cols,&rows,&maxval);
+    //  printf("max_value %d\n",maxval );
         for(row = 0; row < rows; row++){
           for (col = 0; col < cols; col++) {
             // 255 -> Branco
