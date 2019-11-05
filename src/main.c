be15615eb041c337738a68b906be4a04df07ab54
51Megamind <alexrone.rone3@gmail.com>
2019-11-05 16:46:15 +0000

    xD

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int min_vizinhos( gray **matrix, int rows, int cols, int row, int col, int* flag
             if(row + i > 0 && row + i < rows)
                 if(col + j > 0 && col + j < cols)
                     if(matrix[row + i][col + j] < min){
-                        if(!(*flag))
+                        if(!(*flag) )
                             *flag = 1;
                         min = matrix[row + i][col + j];
                     }
@@ -24,11 +24,12 @@ int min_vizinhos( gray **matrix, int rows, int cols, int row, int col, int* flag
 
 int main(int argc, char const *argv[]) {
     unsigned int rows, cols, maxval, row, col, min;
-    int flag = 1;
+    int flag = 0;
     gray **matrix;
     pm_init(argv[0], 0);
     matrix = pgm_readpgm(stdin, &cols,&rows,&maxval);
     gray **output = pgm_allocarray(cols, rows);
+    int iterator = 1;
 
     for(row = 0; row < rows; row++){
         for (col = 0; col < cols; col++) {
@@ -37,24 +38,28 @@ int main(int argc, char const *argv[]) {
     }
 
     while (flag){
-    
         flag = 0;
         for(row = 0; row < rows; row++){
             for (col = 0; col < cols; col++) {
+              if(iterator)
                 output[row][col] = min_vizinhos(matrix, rows, cols, row,col,&flag);
+              else
+                matrix[row][col] = min_vizinhos(output, rows, cols, row,col,&flag);
+
             }
         }
-        for(row = 0; row < rows; row++)
-            for (col = 0; col < cols; col++)
-                matrix[row][col] = output[row][col];
+        iterator = !iterator;
     }
-    
-    
+
+
     FILE *fptr;
     if ((fptr = fopen("testeoutput.pgm","w+")) == NULL){
         printf("Erro ao abrir ficheiro");
         exit(1);
     }
-    pgm_writepgm(fptr, output, cols, rows, maxval, 1);
+    if(iterator)
+        pgm_writepgm(fptr, matrix, cols, rows, maxval, 1);
+    else
+        pgm_writepgm(fptr, output, cols, rows, maxval, 1);
 
 }
