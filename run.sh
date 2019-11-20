16b9b71e2fc7324e62cc5c9e8ec0eae59a784620
62random <josepedropinto98@gmail.com>
2019-11-20 09:43:49 +0000

    AAAA

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
 
-export OMP_NUM_THREADS=$2
 make
-rm *.pgm
-./bin/executavel.o < bin/teste$1.pgm 
+
+for i in 1 2 4
+do
+    echo "######EXECUTANDO COM $i CORES######"
+    echo "###################################"
+    for j in 1 2
+    do
+        export OMP_NUM_THREADS=$i
+        rm *.pgm
+        echo "Executando para o teste $j"
+        ./bin/executavel.o < bin/teste$j.pgm
+    done
+done
