0f7d03b12d2ba77836d18f3509aac2c59acbfaba
62random <josepedropinto98@gmail.com>
2019-11-20 10:15:54 +0000

    aaaaaaaaaaaaa

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 make
 
-for i in 1 2 4
+for i in 1 2 4 8 16 24 32 48 64 96
 do
     echo "######EXECUTANDO COM $i CORES######"
     echo "###################################"
-    for j in 1 2
+    for j in 1 2 3
     do
         export OMP_NUM_THREADS=$i
         rm *.pgm
